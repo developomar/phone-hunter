0d679ab6955d9328c756bda64a1e7ac47e7a20bf
developomar <tasfiyajan2009@gmail.com>
2024-02-27 21:50:45 +0600

    creat phone dynamicly with js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,29 @@ const loadPhone = async () =>{
     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const data = await res.json();
     const phones = data.data;
+    displayPhones(phones)
 
+}
+
+const displayPhones = phones => {
+    phones.forEach(phone => {
+        console.log(phone)
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
+        phoneCard.innerHTML = `
+                    <figure>
+                        <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+        `
+
+    })
 }
 
 loadPhone();
