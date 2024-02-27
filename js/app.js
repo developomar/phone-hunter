173596b12e402cb849cdd9cb81ea9b45379d7082
developomar <tasfiyajan2009@gmail.com>
2024-02-27 21:33:34 +0600

    acces phone data with js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log('connected')
+const loadPhone = async () =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    const phones = data.data;
+
+}
+
+loadPhone();
