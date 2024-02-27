a4303029a08bf5ed2ed933526ea4d033928a2fa1
developomar <tasfiyajan2009@gmail.com>
2024-02-27 22:55:04 +0600

    added item cards dynamicly and added search input

--- js/app.js
+++ js/app.js
@@ -7,22 +7,25 @@ const loadPhone = async () =>{
 }
 
 const displayPhones = phones => {
+    const phoneContainer = document.getElementById('phone-container')
     phones.forEach(phone => {
         console.log(phone)
         const phoneCard = document.createElement('div');
-        phoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
+        phoneCard.classList = `card w-96 bg-white shadow-xl p-4 w-full`;
         phoneCard.innerHTML = `
                     <figure>
-                        <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+                        <img src="${phone.image}" alt="Shoes" />
                     </figure>
                     <div class="card-body">
-                        <h2 class="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div class="card-actions justify-end">
-                            <button class="btn btn-primary">Buy Now</button>
+                        <h2 class="card-title">${phone.phone_name}</h2>
+                        <p>${phone.slug}</p>
+                        <div class="card-actions justify-center">
+                            <button class="btn btn-primary px-6 py-2 text-white text-[1.25rem] font-semibold bg-[#0D6EFD] border-0 hover:bg-[#0d6dfdf3]">Show Details</button>
                         </div>
                     </div>
         `
+
+        phoneContainer.appendChild(phoneCard)
 
     })
 }
